cc8f61fd319bf27406b1243fd18a329a3a73e801
janmurer <jan.murer@bluewin.ch>
2024-03-24 10:16:25 +0100

    I created a little bar plot - however it needs to be formatted correctly in order to be useful

--- scripts/main.R
+++ scripts/main.R
@@ -1,10 +1,24 @@
 library(tidyverse)
 library(here)
 library(readr)
+library(ggplot2)
+
 
 # Load data using here
 billionaires <- here("data_prep", "df_ready.csv") %>%
   read_csv()
 
-# View the data
-View(billionaires)
+# Aggregate the data to count the number of billionaires by industry
+industry_counts <- billionaires %>%
+  group_by(industry) %>%
+  summarise(num_billionaires = n_distinct(full_name))
+
+# Create a bar plot
+ggplot(industry_counts, aes(x = industry, y = num_billionaires)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Industry", y = "Number of Billionaires", title = "Distribution of Billionaires by Industry")
+
+# Save the plot
+
+ggsave(here("output", "billionaires_by_industry.png"))
+
