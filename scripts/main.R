b650cf684aaa61dc60b6c04b811bd324381865a0
nilsohn <nils.brun@stud.unilu.ch>
2024-03-26 07:50:03 +0100

    added a plot that shows the wealth of the billionaires plotted against the tax rate of their country they currently recide in

--- scripts/main.R
+++ scripts/main.R
@@ -31,18 +31,30 @@ swiss_industry_counts <- swiss_billionaires %>%
 ggplot(industry_counts, aes(x = reorder(industry, -num_billionaires), y = num_billionaires)) +
   geom_bar(stat = "identity") +
   labs(x = "Industry", y = "Number of Billionaires", title = "Distribution of Billionaires by Industry") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Save the plot
+
+ggsave(here("output", "billionaires_by_industry.png"))
 
 
 # Create the same plot but only for Swiss billionaires
 
 ggplot(swiss_industry_counts, aes(x = reorder(industry, -num_swiss_billionaires), y = num_swiss_billionaires)) +
   geom_bar(stat = "identity") +
-  labs(x = "Industry", y = "Number of Billionaires", title = "Distribution of Billionaires by Industry") +
+  labs(x = "Industry", y = "Number of Billionaires", title = "Distribution of swiss Billionaires by Industry") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
 
 
 # Save the plot
 
-ggsave(here("output", "billionaires_by_industry.png"))
+ggsave(here("output", "swiss_billionaires_by_industry.png"))
+
+
+# create a Plot comparing the wealth of billionaires by the tax rate of their country
+
+ggplot(billionaires, aes(x = tax_rate, y = wealth, color = country_lat)) +
+  geom_point() +
+  labs(x = "Tax Rate", y = "Wealth", title = "Wealth of Billionaires by Tax Rate") +
+  theme(legend.position = "bottom")
 
